32922e3364e235950e9a6399b9f986312990ddc1
Daniel Bennington <dbennington125@gmail.com>
2020-06-15 16:36:01 -0700

    get by id passes

--- __tests__/auctions.test.js
+++ __tests__/auctions.test.js
@@ -5,6 +5,9 @@ const connect = require('../lib/utils/connect');
 
 const request = require('supertest');
 const app = require('../lib/app');
+const User = require('../lib/models/User');
+const Auction = require('../lib/models/Auction');
+const Bid = require('../lib/models/Bid');
 
 describe('auction routes', () => {
   beforeAll(async() => {
@@ -15,13 +18,85 @@ describe('auction routes', () => {
   beforeEach(() => {
     return mongoose.connection.dropDatabase();
   });
+  
+  let user;
+  beforeEach(async() => {
+    user = await User.create({
+      email: 'yep@no.com',
+      password: 'donotshow'
+    });
+  });
+  let auction; 
+  beforeEach(async() => {
+    auction = await Auction.create({
+      user: user.id,
+      title: 'Stuff',
+      description: 'This is some stuff',
+      quantity: 2,
+      expires: Date.now()
+    });
+  });
+  let bid;
+  beforeEach(async() => {
+    bid = await Bid.create({
+      auction: auction.id,
+      user: user.id,
+      price: 12,
+      quantity: 2, 
+      accepted: false
+      
+    });
+  });
 
   afterAll(async() => {
     await mongoose.connection.close();
     return mongod.stop();
   });
 
-  it('will pass the test', () => {
+  it('makes an auction via POST', () => {
+    return request(app)
+      .post('/api/v1/auctions')
+      .auth('yep@no.com', 'donotshow')
+      .send({
+        user: user.id,
+        title: 'Stuff',
+        description: 'This is some stuff',
+        quantity: 2,
+        expires: Date.now()
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.anything(),
+          user: user.id,
+          title:'Stuff',
+          description: 'This is some stuff',
+          quantity: 2,
+          expires: expect.anything()
+        });
+      });
+  });
 
+  it('gets an auction by ID via GET', async() => {
+    const auction = await Auction.create({
+      user: user.id,
+      title: 'Stuff',
+      description: 'This is some stuff',
+      quantity: 2,
+      expires: Date.now()
+    });
+    return request(app)
+      .get(`/api/v1/auctions/${auction._id}`)
+      .auth('yep@no.com', 'donotshow')
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.anything(),
+          bids: [],
+          user: { _id: expect.anything(), email: 'yep@no.com' },
+          title:'Stuff',
+          description: 'This is some stuff',
+          quantity: 2,
+          expires: expect.anything()
+        });
+      });
   });
 });
